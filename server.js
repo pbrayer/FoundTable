8faf2c142eabe23b0cded08ce89a264a7bc20718
pbrayer <skellocrusha@yahoo.com>
2019-07-08 20:08:30 -0500

    added server to root

---
+++ server.js
@@ -0,0 +1,70 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const session = require('express-session');
+
+
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+const db = require("./models");
+
+// Static directory
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Add session support
+app.use(session({  
+  secret: process.env.SESSION_SECRET || 'default_session_secret',
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());  
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {  
+  done(null, user);
+});
+
+passport.deserializeUser((userDataFromCookie, done) => {  
+  done(null, userDataFromCookie);
+});
+
+// Set up passport strategy
+passport.use(new GoogleStrategy(  
+  {
+    clientID: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_SECRET,
+    callbackURL: 'https://foundtable.herokuapp.com/auth/google/callback',
+    scope: ['email'],
+  },
+  // This is a "verify" function required by all Passport strategies
+  (accessToken, refreshToken, profile, cb) => {
+    console.log('Our user authenticated with Google, and Google sent us back this profile info identifying the authenticated user:', profile);
+    return cb(null, profile);
+  },
+));
+
+// Routes
+// =============================================================
+require("./routes/routes")(app);
+  
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
